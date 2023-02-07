1167df917bce7b9807d110c44466726e6a9a1259
centmount <78281830+centmount@users.noreply.github.com>
2023-02-07 18:20:48 +0900

    Update kentei_data.py

--- kentei_data.py
+++ kentei_data.py
@@ -16,23 +16,26 @@ button= st.button('処理を実行')
 
 if files and button:
     for file in files:
-        df = pd.read_csv(file, encoding='shift-jis')
-        category = df.loc[0, '名称']
-        date = df.loc[0, '登録日時'][2:12]
-        df_color = round(df['解答色'].value_counts() / df['解答色'].count(), 4) * 100
-        df_color = pd.DataFrame(df_color)
-        df_color.index = ['青', '赤', '緑']
-        df_color.columns = [category + '  ' + date]
-        st.dataframe(df_color.T.style.format('{:.2f}'))
-        fig = plt.figure(figsize=(3,3))
-        labels = ["青", "赤", "緑"]
-        colors = ["blue", "red", "green"]
-        textprops = textprops = {'fontsize':8}
-        x = df_color.to_numpy().reshape(-1)
-        plt.pie(x, labels=labels, colors=colors, counterclock=False, startangle=90, autopct="%.2f%%", textprops=textprops)
-        plt.title(category + '  ' + date, fontsize=10)
-        plt.show()
-        buf = BytesIO()
-        fig.savefig(buf, format="png")
-        st.image(buf, width=300)
+        try:
+            df = pd.read_csv(file, encoding='shift-jis')
+            category = df.loc[0, '名称']
+            date = df.loc[0, '登録日時'][2:12]
+            df_color = round(df['解答色'].value_counts() / df['解答色'].count(), 4) * 100
+            df_color = pd.DataFrame(df_color)
+            df_color.index = ['青', '赤', '緑']
+            df_color.columns = [category + '  ' + date]
+            st.dataframe(df_color.T.style.format('{:.2f}'))
+            fig = plt.figure(figsize=(3,3))
+            labels = ["青", "赤", "緑"]
+            colors = ["blue", "red", "green"]
+            textprops = textprops = {'fontsize':8}
+            x = df_color.to_numpy().reshape(-1)
+            plt.pie(x, labels=labels, colors=colors, counterclock=False, startangle=90, autopct="%.2f%%", textprops=textprops)
+            plt.title(category + '  ' + date, fontsize=10)
+            plt.show()
+            buf = BytesIO()
+            fig.savefig(buf, format="png")
+            st.image(buf, width=300)
+        except Exception as e:
+            st.write('ファイルが正しくない可能性があります')                       
     st.write('処理を実行しました')
